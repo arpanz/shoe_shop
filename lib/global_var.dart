ffd8d867cb819fac4c1527c98774b1501141598b
arpanz <arpansinghbsb@gmail.com>
2025-01-21 00:09:34 +0530

    *

---
+++ lib/global_var.dart
@@ -0,0 +1,36 @@
+final products = [
+  {
+    'id': 0,
+    'titile': 'Nike Air Max 90',
+    'price': 120,
+    'size': [8, 9, 10],
+    'company': 'Nike',
+    'image': 'assets/images/1.webp',
+  },
+  {
+    'id': 1,
+    'title': 'Adidas Ultraboost 22',
+    'price': 15000,
+    'size': [7, 8, 9, 10],
+    'company': 'Adidas',
+    'image': 'assets/images/2.webp',
+  },
+  {
+    'id': 2,
+    'title': 'Puma RS-X³',
+    'price': 11000,
+    'size': [8, 9, 10, 11],
+    'company': 'Puma',
+    'image': 'assets/images/3.webp',
+  },
+];
+final cart = [
+  {
+    'id': 2,
+    'title': 'Jordan rodeo',
+    'price': 11000,
+    'size': [8, 9, 10, 11],
+    'company': 'Nike',
+    'image': 'assets/images/13.avif',
+  },
+];
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ['All', 'Adidas', 'Nike', 'Puma'];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey,
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(40),
+      ),
+    );
+    return Scaffold(
       body: SafeArea(
         child: Column(children: [
-          Row(
+          const Row(
             children: [
               Padding(
                 padding: EdgeInsets.all(12.0),
                 child: Text(
                   "Rugged",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
                 ),
               ),
               Expanded(
@@ -22,11 +45,54 @@ class HomePage extends StatelessWidget {
                   decoration: InputDecoration(
                     hintText: "Search",
                     prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
                   ),
                 ),
               ),
             ],
           ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+                itemCount: filters.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedFilter = filter;
+                        });
+                      },
+                      child: Chip(
+                        backgroundColor: selectedFilter == filter
+                            ? Colors.grey
+                            : Color.fromARGB(255, 230, 236, 239),
+                        side: const BorderSide(
+                          color: Colors.transparent,
+                        ),
+                        label: Text(
+                          filter,
+                          style: TextStyle(
+                            fontWeight: selectedFilter == filter
+                                ? FontWeight.w900
+                                : FontWeight.w800,
+                            fontSize: 17,
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(14),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          )
         ]),
       ),
     );
